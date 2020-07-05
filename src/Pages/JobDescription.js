d9cc35413306be42c9b08ccfa5a2b607ea753406
Barnabas Bucsek <bucsekbarnabas@gmail.com>
2020-07-05 11:24:05 +0200

    first commit

---
+++ src/Pages/JobDescription.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JobDescription = () => {
+    return (
+        <div>
+            JOB DESCRIPTIOOOOOOON
+        </div>
+    );
+};
+
+export default JobDescription;
