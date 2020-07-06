86c90fadebb3a3bd0607b7e0bb665ecdaba19af3
Barnabas Bucsek <bucsekbarnabas@gmail.com>
2020-07-06 18:34:05 +0200

    add header component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Route, Switch} from "react-router-dom";
 import SearchPage from "./Pages/SearchPage";
 import PositionDescription from "./Pages/PositionDescription";
 import {SearchResultProvider} from "./Contexts/SearchResultsContext";
+import Header from "./Components/Header";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     return (
         <SearchResultProvider>
             <div className="page-container">
+                <Header/>
                 <Route exact path="/position/:positionId" component={PositionDescription}/>
                 <Route exact path="/" component={SearchPage}/>
             </div>
